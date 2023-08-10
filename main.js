80c435f932d77a8a82ae1cb18b23772c1dba9665
Amanda Santos <113608404+AmandaSoaress@users.noreply.github.com>
2023-08-10 12:36:23 -0300

    ajuste de imagem

--- main.js
+++ main.js
@@ -10,10 +10,10 @@ const listaRecuperada = localStorage.getItem('listaDeItens')
 function atualizaLocalStorage() {
     localStorage.setItem('listaDeItens', JSON.stringify(listaDeItens))
 }
-if(listaRecuperada){
+if (listaRecuperada) {
     listaDeItens = JSON.parse(listaRecuperada)
     mostrarItem()
-}else{
+} else {
     listaDeItens = []
 }
 
@@ -45,28 +45,28 @@ function mostrarItem() {
     listaDeItens.forEach((elemento, index) => {
         if (elemento.checar) {
             ulItensComprados.innerHTML += `
-<li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
-    <div>
-        <input type="checkbox" checked class="is-clickable" />  
-        <span class="itens-comprados is-size-5">${elemento.valor}</span>
-    </div>
-    <div>
-        <i class="fa-solid fa-trash is-clickable deletar"></i>
-    </div>
-</li>`
-        } else {
-            ulItens.innerHTML += `  <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
-<div>
-    <input type="checkbox" class="is-clickable" />
-    <input type="text" class="is-size-5" value="${elemento.valor}" ${index !== Number(itemAEditar) ? 'disabled': ''}></input>
-</div>
+                <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+                    <div>
+                        <input type="checkbox" checked class="is-clickable" />  
+                        <span class="itens-comprados is-size-5">${elemento.valor}</span>
+                    </div>
+                    <div>
+                        <i class="fa-solid fa-trash is-clickable deletar"></i>
+                    </div>
+                </li>`
+                        } else {
+                            ulItens.innerHTML += `  <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+                <div>
+                    <input type="checkbox" class="is-clickable" />
+                    <input type="text" class="is-size-5" value="${elemento.valor}" ${index !== Number(itemAEditar) ? 'disabled': ''}></input>
+                </div>
 
-<div>
-${index === Number (itemAEditar) ? '<button onclick="salvarEdicao()"><i class="fa-regular fa-floppy-disk is-clickable"></i></button>': '<i class="fa-regular is-clickable fa-pen-to-square editar"></i>'}
-    <i class="fa-solid fa-trash is-clickable deletar"></i>
-</div>
-</li>
-`
+                <div>
+                ${index === Number (itemAEditar) ? '<button onclick="salvarEdicao()"><i class="fa-regular fa-floppy-disk is-clickable"></i></button>': '<i class="fa-regular is-clickable fa-pen-to-square editar"></i>'}
+                    <i class="fa-solid fa-trash is-clickable deletar"></i>
+                </div>
+                </li>
+            `
         }
     })
     const inputsCheck = document.querySelectorAll('input[type="checkbox"]')
